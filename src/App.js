7b090ade95c32fa93519037f3708681b10cb7e39
Natasha <natallia.prymost@gmail.com>
2024-02-06 15:26:15 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from './pages';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+//Switch renders the first <Route> child that matches the path
+
+function App() {
+  return (
+    <AuthWrapper>
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <PrivateRoute>
+                <Dashboard />
+              </PrivateRoute>
+            }
+          />
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Router>
+    </AuthWrapper>
+  );
+}
+
+export default App;
---
+++ src/components/Charts/Column3D.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import ReactFC from 'react-fusioncharts';
+
+import FusionCharts from 'fusioncharts';
+
+import Chart from 'fusioncharts/fusioncharts.charts';
+
+import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
+
+ReactFC.fcRoot(FusionCharts, Chart, FusionTheme);
+
+const ChartComponent = ({ data }) => {
+  const chartConfigs = {
+    type: 'column3d', // The chart type
+    width: '100%', // Width of the chart
+    height: '400', // Height of the chart
+    dataFormat: 'json', // Data type
+    dataSource: {
+      chart: {
+        caption: 'Most Popular',
+        yAxisName: 'Stars',
+        xAxisName: 'Repos',
+        xAxisNameFontSize: '16px',
+        yAxisNameFontSize: '16px',
+      },
+      data,
+    },
+  };
+
+  return <ReactFC {...chartConfigs} />;
+};
+
+export default ChartComponent;
